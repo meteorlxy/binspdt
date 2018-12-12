23440ecddb50db566a42bba7305053f891c42259
Xinyu Liu <meteor.lxy@foxmail.com>
2018-12-12 14:33:40 +0800

    chore($client): refine notfound page

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    404 Not Found
-  </div>
+  <VContainer
+    fluid
+    fill-height>
+    <VLayout
+      align-center
+      justify-center>
+      <div class="text-xs-center">
+        <h1 class="error-page-title">
+          404 Not Found
+        </h1>
+
+        <p class="error-page-subtitle mt-3">
+          <RouterLink :to="{ name: 'home' }">
+            Go back home
+          </RouterLink>
+        </p>
+      </div>
+    </VLayout>
+  </VContainer>
 </template>
 
 <script lang="ts">
@@ -10,3 +26,10 @@ import { Vue, Component } from 'vue-property-decorator'
 @Component
 export default class NotFound extends Vue {}
 </script>
+
+<style lang="stylus" scoped>
+.error-page-title
+  font-size 60px
+.error-page-subtitle
+  font-size 25px
+</style>
